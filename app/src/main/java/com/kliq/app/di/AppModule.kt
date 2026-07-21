0b66c02754d46af784759f6172876e95a43b27cf
Mking2001 <1312mkn@gmail.com>
2026-07-21 11:08:43 +0200

    feat(di): update AppModule to provide new DAOs and DB migration

--- app/src/main/java/com/kliq/app/di/AppModule.kt
+++ app/src/main/java/com/kliq/app/di/AppModule.kt
@@ -3,6 +3,9 @@ package com.kliq.app.di
 import android.content.Context
 import androidx.room.Room
 import com.kliq.app.data.local.KliqDatabase
+import com.kliq.app.data.local.dao.ChatDao
+import com.kliq.app.data.local.dao.ClubDao
+import com.kliq.app.data.local.dao.ReviewDao
 import com.kliq.app.data.local.dao.UserDao
 import com.kliq.app.data.remote.KliqApiService
 import dagger.Module
@@ -25,11 +28,22 @@ object AppModule {
             context,
             KliqDatabase::class.java,
             "kliq_db"
-        ).build()
+        )
+        .addMigrations(KliqDatabase.MIGRATION_1_2)
+        .build()
     }
 
     @Provides
     fun provideUserDao(database: KliqDatabase): UserDao = database.userDao()
+
+    @Provides
+    fun provideClubDao(database: KliqDatabase): ClubDao = database.clubDao()
+
+    @Provides
+    fun provideReviewDao(database: KliqDatabase): ReviewDao = database.reviewDao()
+
+    @Provides
+    fun provideChatDao(database: KliqDatabase): ChatDao = database.chatDao()
 
     @Provides
     @Singleton
